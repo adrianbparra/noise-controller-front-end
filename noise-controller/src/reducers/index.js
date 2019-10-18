ee05883065655e671c645411b59e55d70cf4d2cb
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2019-10-17 22:26:05 -0500

    Begin Work on Reducers Files

--- noise-controller/src/reducers/index.js
+++ noise-controller/src/reducers/index.js
@@ -1,4 +1,8 @@
 // React 2 stuff so dont worry about this if React 1 
+
+//Need to figure out how mnay folders will branch out for here
+
+//to create the payloads
 import { initialState } from "../stores";
 
 export const reducer = () => {
