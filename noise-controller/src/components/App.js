fdc81d07058b8142d54dcef127431d627cb5fcb1
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-10-17 22:32:39 -0500

    most of the form is done. it is missing the POST URL and i cant seem to get the select working

--- noise-controller/src/components/App.js
+++ noise-controller/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css';
+import Signup from './Signup';
 
 function App() {
   return (
     <div className="App">
     <h1>NOISE!!</h1>
+    <Signup />
     </div>
   );
 }
--- noise-controller/src/components/Signup.js
+++ noise-controller/src/components/Signup.js
@@ -1,1 +1,117 @@
-import React from 'react';
+import React, { useState } from "react";
+import { axiosWithAuth } from "../data/axiosAuth";
+
+const Signup = props => {
+
+const [signupCreds, setSignupCreds] = useState({
+    username: "",
+    password: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    title: "",
+    err: null
+  });
+
+  const handleChange = e => {
+    setSignupCreds({
+      ...signupCreds,
+      [e.target.name]: e.target.value,
+      err: null
+    });
+  };
+
+  const signup = () => {
+    axiosWithAuth()
+      .post(``, {
+        username: signupCreds.username,
+        password: signupCreds.password,
+        email: signupCreds.email,
+        title: signupCreds.title,
+        firstName: signupCreds.firstName,
+        lastName: signupCreds.lastName,
+      })
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/home");
+      })
+      .catch(err =>
+        setSignupCreds({
+          ...signupCreds,
+          err: "Error logging in. Please try again."
+        })
+      );
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    signupCreds.username === "" || signupCreds.password === ""
+      ? setSignupCreds({
+          ...signupCreds,
+          err: "Please complete all login fields."
+        })
+      : signup();
+  };
+
+  return (
+    <div className="signup-page">
+    <form>
+        <h4>Sign Up</h4>
+        {/* <select 
+          name="title"
+          onChange={handleChange}
+        />
+            <option value={signupCreds.title}>Mr.</option>
+            <option value={signupCreds.title}>Mrs.</option>
+            <option value={signupCreds.title}>Ms.</option>
+        </select> */}
+        {/* <input
+          type="select"
+          name="title"
+          onChange={handleChange}
+        >
+            <option value={signupCreds.title}>Mr.</option>
+            <option value={signupCreds.title}>Mrs.</option>
+            <option value={signupCreds.title}>Ms.</option>
+        </input> */}
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Enter first name..."
+          value={signupCreds.firstName}
+          onChange={handleChange}
+          autoComplete="first name"
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Enter last name..."
+          value={signupCreds.lastName}
+          onChange={handleChange}
+          autoComplete="last name"
+        />
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter username..."
+          value={signupCreds.username}
+          onChange={handleChange}
+          autoComplete="username"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password..."
+          value={signupCreds.password}
+          onChange={handleChange}
+          autoComplete="current-password"
+        />
+        <button onClick={handleSubmit}>Sign Up</button>
+        {signupCreds.err && (
+          <div className="error-container">{signupCreds.err}</div>
+        )}
+      </form>
+    </div>
+  );
+};
+export default Signup;
