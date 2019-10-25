17298a75aa52971244e4e75ce2b45e7ad9d786e7
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-10-24 22:07:48 -0500

    not working yet

--- noise-controller/src/components/App.js
+++ noise-controller/src/components/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import '../App.css';
+import jwtDecode from 'jwt-decode';
+import ScoresList from './ScoresList';
+import Home from './Home';
+import axiosWithAuth from '../axiosWithAuth';
 
 function App() {
+
   return (
     <div className="App">
-    <h1>NOISE!!</h1>
+
+  <ScoresList />
+
+    {/* <Route
+      exact
+      path='/scorelist/:id'
+      component = {props => <ScoresList {...props} teachers={teachers} />}
+    /> */}
+
+
     </div>
   );
 }
--- noise-controller/src/components/Home.js
+++ noise-controller/src/components/Home.js
@@ -1,1 +1,15 @@
 // where the actual animals are dissapearing and where we are measuring mic sensitivity 
+import React, { useState } from 'react';
+import ScoresList from './ScoresList';
+
+function Home(props) {
+
+
+    return (
+        <div>
+            <ScoresList  />
+        </div>
+    );
+}
+
+export default Home;
