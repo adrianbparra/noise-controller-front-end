e52bb32ccc347779a8dee4f0e9806ff93a440547
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-10-28 17:14:57 -0500

    added settings still needs styling

--- noise-controller/src/components/Settings.js
+++ noise-controller/src/components/Settings.js
@@ -1,1 +1,54 @@
-// where teacher can adjust microphone sensitivity and the amount of time in takes to switch animals. Turnary.
+import React from 'react';
+import '../Settings.css';
+
+const Settings = props => {
+
+
+    return (
+        <div className='settings-container'>
+            <h1>Settings</h1>
+            <div className='settings-row'>
+                
+                <form>
+                    <div>
+                        <label>
+                            Mic Sensitivity
+                        </label>
+                            <select>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                <option value='5'>5</option>
+                                <option value='6'>6</option>
+                                <option value='7'>7</option>
+                                <option value='8'>8</option>
+                                <option value='9'>9</option>
+                                <option value='10'>10</option>
+                            </select>
+                    </div>
+                    <div>
+                        <label>
+                            Time between switching animals
+                        </label>
+                            <select>
+                                <option value='10'>10 secs</option>
+                                <option value='20'>20 secs</option>
+                                <option value='30'>30 secs</option>
+                                <option value='40'>40 secs</option>
+                                <option value='50'>50 secs</option>
+                                <option value='60'>60 secs</option>
+                                <option value='70'>70 secs</option>
+                                <option value='80'>80 secs</option>
+                                <option value='90'>90 secs</option>
+                                <option value='100'>100 secs</option>
+                            </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+
+export default Settings;
