d740e0b35dd7be0348f936bcbde2abaede1c1162
adrianbparra <adrianplane12@gmail.com>
2021-02-10 16:59:15 -0700

    Updated component to show scores
    Updated component to show selected class scores. Url changes and scores change based on what is the selected class. Table does not contain streak for each score.

--- src/components/ScoresList.js
+++ src/components/ScoresList.js
@@ -1,44 +1,30 @@
 // Where we will render classroom daily scores (multiple daily scores from the entire week maybe? depends on data..)
 import React, { useState, useEffect } from 'react';
 import {useHistory} from "react-router-dom"
-
-import ScoreCard from './ScoreCard';
 import { Table, Segment, Header } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+
+import {SELECTEDCLASS} from "../queries/queries";
+import ScoreCard from './ScoreCard';
 
 const ScoresList = props => {
+    const { data } = useQuery(SELECTEDCLASS);
+    // const { selectedClass }= data.getUser;
+    console.log(data)
     const history = useHistory();
 
-    // useEffect(() => {
-    //     console.log("class  change")
-    //     if(props.selectedClass.name && props.match.params.name !== props.selectedClass.name){
-    //         props.getScores(props.selectedClass.id)
-    //         let path =  "/" + props.selectedClass.name + "/scores";
-    //         history.push(path, props.selectedClass)
-    //     }
-    // }, [props.selectedClass]);
-
-    useEffect(()=> {
-        console.log("match change", props.match.params)
-        
-        let [classUrl] = props.classes.filter(cls => cls.name === props.match.params.name)
-        
-        if(props.match.params.name !== props.selectedClass.name){
-                console.log(props.match.params)
-            
-                props.selectCurrentClass(classUrl)
-            } else {
-                
+    useEffect(() => {
+        console.log("class  change")
+        if(data && props.match.params.name !== data.getUser.selectedClass.name){
+            let pathname =  "/" + data.getUser.selectedClass.name + "/scores";
+            history.replace({ pathname })
         }
-
-    },[props.match.params])
-
-
-    ///refresh page is props.selectedClass update
+    }, [data]);
 
     return (
         <Segment>
             <Header as="h2" textAlign="center">
-               Scores for {props.selectedClass.name}
+               Scores for {data&& data.getUser.selectedClass.name}
             </Header>
             <Table celled>
                 <Table.Header>
@@ -46,12 +32,11 @@ const ScoresList = props => {
                         <Table.HeaderCell>Date</Table.HeaderCell>
                         <Table.HeaderCell>Theme</Table.HeaderCell>
                         <Table.HeaderCell>Score</Table.HeaderCell>
-                        <Table.HeaderCell>Streak</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    {props.scores ? props.scores.map(score => <ScoreCard score={score} />): null}
+                    {data ? data.getUser.selectedClass.scores.map(score => <ScoreCard score={score} />): null}
 
                 </Table.Body>
 
