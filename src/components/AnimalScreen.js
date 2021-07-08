cfdb9a0307f6a5ed2577f19623eb5bb4e2531e45
adrianbparra <adrianplane12@gmail.com>
2021-07-07 18:05:26 -0700

    Create AnimalScreen.js
    Start of the animal screen to display animals

---
+++ src/components/AnimalScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+
+
+function AnimalScreen(props) {
+    return (
+        <Segment>
+            {/* shows animals depending on time counted and voice low */}
+        </Segment>
+    );
+}
+
+export default AnimalScreen;
