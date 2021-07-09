4ce6f96a5071b61f5e70d909930a789ea749fe05
adrianbparra <adrianplane12@gmail.com>
2021-07-08 21:00:34 -0700

    Animal screen style
    Added style to animal screen to display as container.

--- src/components/AnimalScreen.js
+++ src/components/AnimalScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
+import styled from "styled-components";
 
+const GameScreenSegment = styled.div`
+    height:auto;
+    width: 95%;
+    border: 1px solid rgba(34,36,38,.15);
+    border-radius: .28571429rem;
+    margin-left: auto;
+    margin-right: auto;
+
+`;
 
 function AnimalScreen(props) {
     return (
-        <Segment>
+        <GameScreenSegment>
+            Game Screen
             {/* shows animals depending on time counted and voice low */}
-        </Segment>
+        </GameScreenSegment>
     );
 }
 
