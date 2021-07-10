5c0e540a8bde765dddc617b56534997921333f06
adrianbparra <adrianplane12@gmail.com>
2021-07-09 19:09:10 -0700

    bg image style
    Added background image and styled it.

--- src/components/AnimalScreen.js
+++ src/components/AnimalScreen.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
 import styled from "styled-components";
+import backgroundImage from "../images/farm_background.png"
 
 const GameScreenSegment = styled.div`
-    height:auto;
     width: 95%;
+    height: 95%;
     border: 1px solid rgba(34,36,38,.15);
     border-radius: .28571429rem;
     margin-left: auto;
     margin-right: auto;
+    background-color: #9fc5e8ff
+`;
 
+const  Image = styled.img`
+    max-width: 100%;
+    max-height: 100%;
+    width: 100%;
+    object-fit: cover;
 `;
 
 function AnimalScreen(props) {
     return (
         <GameScreenSegment>
-            Game Screen
-            {/* shows animals depending on time counted and voice low */}
+            <Image src={backgroundImage} alt="Farm Background"/>
         </GameScreenSegment>
     );
 }
