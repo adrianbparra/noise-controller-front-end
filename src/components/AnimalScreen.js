f07bcd25441fc7b55a5e77a5471ae65ad5f12ec9
adrianbparra <adrianplane12@gmail.com>
2021-07-10 16:35:45 -0700

    Update AnimalScreen.js
    Updated animal screen style to fit image properly

--- src/components/AnimalScreen.js
+++ src/components/AnimalScreen.js
@@ -3,28 +3,24 @@ import { Grid, Segment } from 'semantic-ui-react';
 import styled from "styled-components";
 import backgroundImage from "../images/farm_background.png"
 
-const GameScreenSegment = styled.div`
-    width: 95%;
-    height: 95%;
+const GameScreenDivStyle = styled.div`
     border: 1px solid rgba(34,36,38,.15);
     border-radius: .28571429rem;
-    margin-left: auto;
-    margin-right: auto;
-    background-color: #9fc5e8ff
+    background-color: #9fc5e8ff;
 `;
 
-const  Image = styled.img`
+const  ImageStyle = styled.img`
     max-width: 100%;
     max-height: 100%;
-    width: 100%;
-    object-fit: cover;
+    height: auto;
+    display: block;
 `;
 
 function AnimalScreen(props) {
     return (
-        <GameScreenSegment>
-            <Image src={backgroundImage} alt="Farm Background"/>
-        </GameScreenSegment>
+        <GameScreenDivStyle>
+            <ImageStyle src={backgroundImage} alt="Farm Background"/>
+        </GameScreenDivStyle>
     );
 }
 
