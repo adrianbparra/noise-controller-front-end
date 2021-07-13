110a94d3d5dbd5929cc5006e806a4fccf9af05a5
adrianbparra <adrianplane12@gmail.com>
2021-07-12 18:35:44 -0700

    Update AudioMeter.js
    Updated style for audio meter on changing based on value 0-100

--- src/components/AudioMeter.js
+++ src/components/AudioMeter.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import {Segment} from "semantic-ui-react"
+import {Progress, Segment} from "semantic-ui-react"
 import styled from "styled-components";
 
 const AudioContainerStyle = styled.div`
     height: 100%;
     padding: 0!important;
+    min-height: 22vh;
+
+`;
+const AudioMeterStyle = styled.div`
+    width: 100%;
+    display: inline-block;
+    height: ${props => props.height + "%" };
+    background-color: ${props => "hsl("+ (100 - props.color) +", 100%, 50%)"};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+    border-radius: .28571429rem;
+
 `;
 
-function AudioMeter(props) {
+function AudioMeter({range}) {
+    
     return (
         <AudioContainerStyle as={Segment}>
-            
+            <AudioMeterStyle height={75} color={75}/>
         </AudioContainerStyle>
     );
 }
