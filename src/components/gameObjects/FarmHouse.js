f67af13c1006dfe3e5167278580a350b296c1a9d
adrianbparra <adrianplane12@gmail.com>
2022-07-07 18:44:50 -0700

    Update FarmHouse.js
    removed some values that were not used

--- src/components/gameObjects/FarmHouse.js
+++ src/components/gameObjects/FarmHouse.js
@@ -6,7 +6,7 @@ export default function FarmHouse(props) {
     const mesh = useRef();
     const {scene,animations} = useGLTF("/farm_house.gltf", true);
     const { ref, mixer, names, actions, clips } = useAnimations(animations,mesh)
-    // scene.castShadow = true;
+
     useEffect(()=>{
 
         console.log("ref",ref,"mixdr",mixer,"names",names,"action",actions,"clips",clips,scene)
@@ -22,7 +22,7 @@ export default function FarmHouse(props) {
 
     // console.log(animations)
 
-    return <primitive clampWhenFinished={true} {...props} onClick={()=>{openCloseDoor()}} ref={mesh} object={scene} dispose={null} />;
+    return <primitive {...props} onClick={()=>{openCloseDoor()}} ref={mesh} object={scene} dispose={null} />;
 }
 
 useGLTF.preload("/farm_house.gltf")
